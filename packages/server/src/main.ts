b1cc03c63065c4bc802160fa1be4afad129a3fd8
Masanori Ohgita <mp@ohgita.info>
2020-12-29 01:54:40 +0900

    Add: server - OpenAPI document with using Swagger

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api'); // Change the URL prefix to `/api` on backend
+
+  // Change the URL prefix to `/api` on backend
+  app.setGlobalPrefix('api');
+
+  // Build the OpenAPI document (published under `/api/docs`) with Swagger
+  const doc_options = new DocumentBuilder().setTitle(`API Document`).build();
+  const doc = SwaggerModule.createDocument(app, doc_options);
+  SwaggerModule.setup('api/docs', app, doc);
+
+  // Start the server
   await app.listen(3000);
 }
 bootstrap();
