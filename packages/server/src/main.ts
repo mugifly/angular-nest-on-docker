f7dffbc0a6ceaf47278116e6807176b446052629
Masanori Ohgita <mp@ohgita.info>
2020-12-29 04:34:23 +0900

    Add: Production - Deploy to Heroku

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, doc);
 
   // Start the server
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
